1cd9f4e61ef531aaf0da4f6da1410d9f25c9a6ff
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-01-13 21:53:22 -0600

    finished task 6..............

--- index.js
+++ index.js
@@ -282,10 +282,19 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  const yup = { 
+    id: 20,
+    name: 'Your Name Here', 
+    years: 'Your Birth Year - current day',
+    genre: 'Web Design', 
+    nationality: 'Your Nationality Here',
+    bio: 'Add 1-2 sentences (or use lorem ipsum)'
+  }
+  array.push(yup)
+  return array 
 }
-
+console.log(addArtist(artists))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
